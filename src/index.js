301278d9822154d6cea0687dfed0ad6f1f89864c
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-02-22 15:02:44 +0300

    add: algoritm in index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) n = n*-1
+  let a = String(n).split("");
+  let n1 = "";
+  for (let i = a.length -1; i >= 0 ; i--){
+      n1 = n1 + a[i];
+  }
+  return (n1);
 }
